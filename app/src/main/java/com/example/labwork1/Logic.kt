2265ac5ed088bccce215be6525bd4aa4f1229087
admin <dzhem.z@yandex.ru>
2023-09-10 15:26:31 +0300

    SomeCodeUpdates

--- app/src/main/java/com/example/labwork1/Logic.kt
+++ app/src/main/java/com/example/labwork1/Logic.kt
@@ -1,21 +1,13 @@
 package com.example.labwork1
 
-import android.annotation.SuppressLint
-import android.view.View
-import android.widget.EditText
-import android.widget.TextView
-
 object Logic {
 
     fun action(str: String): String {
         val result = str.split(" ")
         return result.zip(result.indices)
-            .filter{ it.first == "0" }
+            .filter { it.first == "0" }
             .unzip()
-            .second
-            .map { it.toString() }
-            .joinToString()
-
+            .second.joinToString { it.toString() }
     }
 
 }
--- app/src/main/java/com/example/labwork1/MainActivity.kt
+++ app/src/main/java/com/example/labwork1/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.labwork1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -13,8 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btn = findViewById<Button>(R.id.action).setOnClickListener {
-            findViewById<TextView>(R.id.ResultRepresent).text = Logic.action(findViewById<EditText>(R.id.textField).text.toString());
+        findViewById<Button>(R.id.action).setOnClickListener {
+            findViewById<TextView>(R.id.ResultRepresent)
+                .text = Logic
+                    .action(findViewById<EditText>(R
+                    .id
+                    .textField)
+                    .text.toString())
         }
     }
 }
